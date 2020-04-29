a681745b5a6f24a068faa250c137ba5be5e96e1d
sumit7541 <sumit7541939580@gmail.com>
2020-04-29 17:50:05 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>facebook.com | connect Together</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+
+<body class="bg-light">
+
+    <nav class="navbar navabar-expand-lg navbar-dark bg-primary">
+        <a href="index.php" class="navbar-brand mt-2">
+            <h5>Facebook</h5>
+        </a>
+
+        <table class="ml-auto">
+            <tr>
+                <th class="small text-white">Username Or Email</th>
+                <th class="small text-white">Password</th>
+            </tr>
+            <tr>
+                <td><input type="text" placeholder="username/email" class="form-control form-control-sm"></td>
+                <td><input type="password" placeholder="password" class="form-control form-control-sm"></td>
+                <td><input type="submit" class="btn btn-info btn-sm"></td>
+            </tr>
+        </table>
+    </nav>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-lg-8">
+                <h1 class="display-4">Welcome in No.1 Social Networking Site</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos itaque, animi ut pariatur impedit numquam rem mollitia incidunt nulla, magni expedita. Est odit vero consequuntur voluptatem, aliquam quasi accusamus consequatur.</p>
+            </div>
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                       <h2 class="blockquote m-0 p-0">Create an Account</h2>
+                       <small class="text-muted">100% Free of Cost</small>
+                        <form action="" class="mt-2">
+                         <div class="row">
+                                <div class="form-group col">
+                                <label for="fname" class="m-0 p-0 text-muted">First Name</label>
+                                <input type="text" id="fname" name="fname" class="form-control">
+                            </div>
+                            <div class="form-group col">
+                                <label for="lname" class="m-0 p-0 text-muted">Last Name</label>
+                                <input type="text" id="lname" name="lname" class="form-control">
+                            </div>
+                         </div>
+                            <div class="form-group">
+                                <label for="email" class="m-0 p-0 text-muted">Email</label>
+                                <input type="email" id="email" name="email" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="contact" class="m-0 p-0 text-muted">Contact</label>
+                                <input type="number" id="contact" name="contact" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="dateofbirth" class="m-0 p-0 text-muted">Date Of Birth</label>
+                                <input type="date" id="dateofbirth" name="dateofbirth" class="form-control">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password" class="m-0 p-0 text-muted">Password</label>
+                                <input type="password" id="password" name="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="create_account" class="btn btn-success btn-block">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
