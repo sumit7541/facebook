24362e341385c629fef5b02c0f6919abe201700e
sumit7541 <sumit7541939580@gmail.com>
2020-04-29 18:38:43 +0530

    first commit

--- profile.php
+++ profile.php
@@ -5,8 +5,48 @@
     <title>facebook.com | Connect together</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
-<body bg-light>
-   <nav class="navbar navbar navbar-expand-lg navbar-dark"></nav>
+<body class="bg-light">
+  <?php include "include/nav.php"; ?>
+   <nav class="navbar navbar navbar-expand-lg navbar-dark bg-primary">
+       <div class="container">
+           <a href="" class="navbar-brand">Facebook</a>
+           <form action="" class="form-inline mx-auto">
+               <input type="search" class="form-control" size="70">
+               <input type="submit" class="btn btn-dark">
+           </form>
+           <ul class="navbar-nav ml-auto">
+               <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+               <li class="nav-item"><a href="" class="nav-link">Profie</a></li>
+               <li class="nav-item"><a href="" class="nav-link">Logout</a></li>
+           </ul>
+       </div>
+   </nav>
+   <div class="container mt-3">
+       <div class="row">
+           <div class="col-lg-3">
+               <div class="card">
+                  <img src="https://via.placeholder.com/250" alt="">
+                   <div class="card-body text-center">
+                      <h2 class="h6 text-uppercase">Sumit Kumar</h2>
+                       
+                   </div>
+               </div>
+           </div>
+           <div class="col-lg-9">
+               <div class="card">
+                  
+                       <form action="">
+                           <textarea name="" id="" rows="6" class="form-control" placeholder="Write Somethong Here"></textarea>
+                       </form>
+                        <div class="card-body">
+                        <input type="file">
+                        <input type="submit" class="btn btn-primary btn-sm float-right">
+                   </div>
+               </div>
+           </div>
+           <div class="col-lg-3"></div>
+       </div>
+   </div>
     
 </body>
 </html>
